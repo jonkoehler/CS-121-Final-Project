60bbf939489ee8679a1254dffa8f10dd23e08fdd
Jon Koehler <jonkoehler15@gmail.com>
2016-12-10 18:43:55 -0500

    Update ColorPicker.py

--- ColorPicker.py
+++ ColorPicker.py
@@ -1,7 +1,3 @@
-# Pin numbers
-RED_PIN   = 17
-GREEN_PIN = 22
-BLUE_PIN  = 24
 
 import os
 import sys
@@ -11,25 +7,26 @@ import pigpio
 import time
 from thread import start_new_thread
 
+# Pin numbers
+RED_PIN   = 17
+GREEN_PIN = 22
+BLUE_PIN  = 24
+
 #global variables 
-bright = 255
-r = 255.0
-g = 0.0
-b = 0.0
-rBright = 0
+bright = 255	#max brightness
+rBright = 0	#values of each color
 gBright = 0
 bBright = 0
-
-
-abort = False         #set the killl switch to false
-
+abort = False         #set the kill switch to false
 pi = pigpio.pi()      #the program it's interacting with 
 
+#This function was taken from an open source script. This was NOT written by us
 #function sets the color of the lights with a value from 0 to 255
 def setLights(pin, brightness):
 	realBrightness = int(int(brightness) * (float(bright) / 255.0))
 	pi.set_PWM_dutycycle(pin, realBrightness)
 
+#This function was taken from an open source script. This was NOT written by us
 #fuction gets the key presses
 def getCh():
 	fd = sys.stdin.fileno()
@@ -41,7 +38,7 @@ def getCh():
 		termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 	return ch
 
-
+#makes sure the values or the colors do not exceed 255 or go less than 0
 def checkBright():
     global rBright 
     global bBright 
@@ -71,7 +68,7 @@ def checkBright():
         bBright = 0
         print ("Blue doesn't go any dimmer!")    
 
-
+#check each keypress
 def checkKey():
 	global abort
         global rBright 
@@ -93,8 +90,7 @@ def checkKey():
 			rBright -= 10
 			checkBright()
 			setLights(RED_PIN, rBright)
-			print ("Red Brightness: "+ str(rBright))
-
+			print ("Red Brightness: "+ str(rBright)
 			
 		if c == 'g':
 			time.sleep(0.01)
@@ -127,10 +123,10 @@ def checkKey():
 		if c == 'c' and not abort:
 			abort = True
 			break
-
+			       
 start_new_thread(checkKey, ())
 
-
+#print user instructions 
 print ("+ / - = Increase / Decrease brightness")
 print ("p / o = Pause / Resume")
 print ("c = Abort Program")
@@ -143,12 +139,7 @@ print ("3 = Decrease Blue")
     
 while abort == False:
     if abort == True:
-        print ("Aborting...")
-
         setLights(RED_PIN, 0)
         setLights(GREEN_PIN, 0)
         setLights(BLUE_PIN, 0)
-
-        time.sleep(0.5)
-
         pi.stop()
